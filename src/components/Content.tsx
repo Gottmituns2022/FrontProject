699ed39014b84749ce30c89ffb1489f16b7ca59c
Владимир Чурин <v.churin2004@mail.ru>
2024-10-27 20:10:06 +0300

    Tested&Structured

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -9,12 +9,12 @@ const Content:FunctionComponent = () =>{
     return(<div className="content">
                 <div className="NowBlock FirstMainUnit"><CurrentDay weatherMinutely = {weatherMinutely}/></div>
                 <div className="MainUnit TodayBlock"><Today weather = {weather.data[0]}/></div>
-                <div className="MainUnit ThirddayBlock"><Day weather = {weather.data[1]} NumberOfDay = {1}/></div>
-                <div className="MainUnit FourthdayBlock"><Day weather = {weather.data[2]} NumberOfDay = {2}/></div>
-                <div className="MainUnit FifthdayBlock"><Day weather = {weather.data[3]} NumberOfDay = {3}/></div>
-                <div className="MainUnit SixthdayBlock"><Day weather = {weather.data[4]} NumberOfDay = {4}/></div>
-                <div className="MainUnit SeventhdayBlock"><Day weather = {weather.data[5]} NumberOfDay = {5}/></div>
-                <div className="MainUnit EighthdayBlock"><Day weather = {weather.data[6]} NumberOfDay = {6}/></div>
+                <div className="MainUnit"><Day weather = {weather.data[1]} NumberOfDay = {1}/></div>
+                <div className="MainUnit"><Day weather = {weather.data[2]} NumberOfDay = {2}/></div>
+                <div className="MainUnit"><Day weather = {weather.data[3]} NumberOfDay = {3}/></div>
+                <div className="MainUnit"><Day weather = {weather.data[4]} NumberOfDay = {4}/></div>
+                <div className="MainUnit"><Day weather = {weather.data[5]} NumberOfDay = {5}/></div>
+                <div className="MainUnit"><Day weather = {weather.data[6]} NumberOfDay = {6}/></div>
            </div>
     );
 }
